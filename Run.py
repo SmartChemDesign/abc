181a63a2bcd14b7df80ac536391ade5d01e0499a
MaximYablonskiy <115433667+MaximYablonskiy@users.noreply.github.com>
2025-03-20 16:27:43 +0300

    Update Run.py
    Add MP2's flags

--- Run.py
+++ Run.py
@@ -6,7 +6,11 @@ from Molecule import HiveMolecule
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Optimize a molecule using Hive.")
+    parser = argparse.ArgumentParser(
+        description="Optimize a molecule using Hive.", 
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    
     parser.add_argument(
         "input",
         type=str,
@@ -38,16 +42,32 @@ def main():
     )
     parser.add_argument(
         "--global_exists",
-        type=str,
-        default='',
+        action='store_true',
         help="Provide the local search in respect to user's global conformation",
     )
+    
+    parser.add_argument(
+        "--MP2",
+        action='store_true',
+        help="flag that enables optimization at the MP2 level of theory",
+    )
+    parser.add_argument(
+    "--basis",
+    default='sto-3g',
+    help="choose basis available in PySCF package",
+    )
 
+    parser.add_argument(
+    "--iters",
+    default=100,
+    help="Number of iterations of final optimization",
+    )
+    
     args = parser.parse_args()
-
+    print(args)
     print(f"Molecule to be optimized: {args.molecule}")
     mol = HiveMolecule(args.input, args.molecule)
-    ensemble = Cycle.full_cycle(mol, args.global_iterations, args.local_iterations, args.global_exists, args.n_confs)
+    ensemble = Cycle.full_cycle(mol, args.global_iterations, args.local_iterations, args.global_exists, args.n_confs, args.MP2, args.basis, args.iters)
     return ensemble
 
 if __name__ == "__main__":
